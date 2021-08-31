262cb52c6619710da20eb4d8ea9b085859985364
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2021-08-31 17:52:50 -0500

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Juan Diego Ocampo on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
