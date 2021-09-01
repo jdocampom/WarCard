4effe5db554d92fc064d2c3818c5bc909c0f21ea
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2021-09-01 10:04:19 -0500

    Lesson 4 Done

--- CardWar/ContentView.swift
+++ CardWar/ContentView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-        // These are the modifiers for the green background and white text
+        VStack(alignment: .center) {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+            HStack(){
+                Text("Hello")
+                Spacer()
+                Text("World!")
+            }
             .padding()
-            .background(Color.green)
-            .cornerRadius(10)
-            .foregroundColor(Color.white)
-        // These are the additional modifiers for the blue background
-            .padding()
-            .background(Color.blue)
-            .cornerRadius(10)
+        }
+        .padding()
     }
 }
 
