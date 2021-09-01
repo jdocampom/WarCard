c80b86aefaf2023c339736075f9e47a413625662
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2021-09-01 10:21:46 -0500

    Challenge 4 Completed

--- CardWar/ContentView.swift
+++ CardWar/ContentView.swift
@@ -9,19 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .center) {
-            Image("logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Spacer()
-            HStack(){
-                Text("Hello")
-                Spacer()
-                Text("World!")
-            }
-            .padding()
+        VStack {
+            // MARK: - CN Tower Card
+            ZStack {
+                /// Tag: Image
+                Image("toronto")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                /// Tag: VStack for the Text
+                VStack(alignment: .center) {
+                    Text("CN Tower")
+                        .font(.largeTitle)
+                        .padding([.top, .leading, .trailing])
+                    Text("Toronto")
+                        .font(.caption)
+                        .padding([.leading, .bottom, .trailing])
+                }
+                /// Tag: VStack Modifiers
+                .background(Color.black)
+                .opacity(0.8)
+                .cornerRadius(10)
+                /// Tag: VStack Element Modifiers
+                .foregroundColor(Color.white)
+                
+            }.padding()
+            
+            // MARK: - Big Ben Card
+            ZStack {
+                /// Tag: Image
+                Image("london")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                /// Tag: VStack for the Text
+                VStack(alignment: .center) {
+                    Text("Big Ben")
+                        .font(.largeTitle)
+                        .padding([.top, .leading, .trailing])
+                    Text("London")
+                        .font(.caption)
+                        .padding([.leading, .bottom, .trailing])
+                }
+                /// Tag: VStack Modifiers
+                .background(Color.black)
+                .opacity(0.8)
+                .cornerRadius(10)
+                /// Tag: VStack Element Modifiers
+                .foregroundColor(Color.white)
+                
+            }.padding()
         }
-        .padding()
     }
 }
 
